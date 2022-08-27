396066e75d11b5729cccf6cbc809426d0b788f90
Calvin Gerling <calvin@gerling.sh>
2022-08-27 10:29:42 -0300

    create application callback module

--- lib/hn_aggregator.ex
+++ lib/hn_aggregator.ex
@@ -1,3 +1,13 @@
 defmodule HNAggregator do
   @moduledoc false
+
+  use Application
+
+  @impl Application
+  def start(_type, _args) do
+    children = []
+
+    opts = [strategy: :one_for_one, name: HNAggregator.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
 end
