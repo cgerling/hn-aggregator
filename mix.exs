ecb408639d5fbccd08b28af10725c42158125318
Calvin Gerling <calvin@gerling.sh>
2022-08-31 10:22:44 -0300

    bump project version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HNAggregator.MixProject do
   def project do
     [
       app: :hn_aggregator,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.13",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
